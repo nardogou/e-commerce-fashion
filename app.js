d3618444054bb2a4829c86395219a0da4aea8768
Andrian Aji <andrian@Andrians-MacBook-Air.local>
2022-06-24 06:05:39 +0700

    add on app

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const session = require('express-session')
 const routes = require("./routes/index");
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
+
+app.use(session({
+  secret: 'Secret ',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: false,
+    sameSite: true
+ }
+}))
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
