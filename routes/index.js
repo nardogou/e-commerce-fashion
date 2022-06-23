5768fba2fee7500dcee92fde2e642c12705f0b07
Andrian Aji <andrian@Andrians-MacBook-Air.local>
2022-06-23 17:51:41 +0700

    coba2

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const Controller = require("../controllers/index");
 const router = express.Router();
 
 router.get("/", Controller.home);
+router.get("/login", Controller.home);
+router.get("/register", Controller.home);
 
 
 
